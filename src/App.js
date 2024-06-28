1f7a7fc1f9d908aa0639c6f43a2a505e248610bb
austinp <auspendleton@gmail.com>
2024-06-27 18:30:20 -0600

    Total calories added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import FoodList from "./components/FoodList";
 import AddFoodForm from "./components/AddFoodForm";
+import TotalCalories from "./components/TotalCalories";
 
 const App = () => {
   const [foods, setFoods] = useState([]);
+  const [totalCalories, setTotalCalories] = useState(0);
+
+  const addFood = (food) => {
+    setFoods([...foods, food]);
+    setTotalCalories(totalCalories + Number(food.calories));
+  };
 
   return (
     <div>
       <Header />
       <AddFoodForm onAddFood={addFood} />
       <FoodList foods={foods} />
+      <TotalCalories total={totalCalories} />
     </div>
   );
 };
---
+++ src/components/TotalCalories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalCalories = ({ total }) => {
+  return (
+    <div>
+      <h2>Total calories: {total} kcal</h2>
+    </div>
+  );
+};
+
+export default TotalCalories;
