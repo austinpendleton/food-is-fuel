1d946b1731b4ffdcdb1b0741a6bf5d97a6dd4c5a
austinp <auspendleton@gmail.com>
2024-07-06 17:44:45 -0600

    Header font trial

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1 className="text-7xl font-bold mb-4 text-center">Food Is Fuel</h1>
+      <h1 className="text-7xl font-bold mb-4 text-center font-playwrite">
+        Food Is Fuel
+      </h1>
     </header>
   );
 };
