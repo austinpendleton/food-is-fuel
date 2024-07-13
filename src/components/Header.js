ea3d736ac0415584dec5f43ad1f85972f4d9c600
austinp <auspendleton@gmail.com>
2024-07-13 16:37:33 -0600

    color theme olive and salmon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <h1 className="text-7xl font-bold mb-4 text-center font-playwrite">
-        Food Is Fuel
-      </h1>
-    </header>
+    <h1 className="text-7xl font-bold mb-4 text-center font-playwrite">
+      Food Is Fuel
+    </h1>
   );
 };
 export default Header;
