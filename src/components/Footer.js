6b7ed9c84f2782d29f9c9dce79da018e3f921002
austinp <auspendleton@gmail.com>
2024-08-08 19:24:02 -0600

    Footer styles updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 text-center">
-      Developed by Austin Pendleton 2024
+    <footer className="w-full py-4 flex justify-between items-center">
+      <div className="ml-4 whitespace-nowrap">
+        Developed by Austin Pendleton
+      </div>
+      <div className="mr-4 whitespace-nowrap">2024</div>
     </footer>
   );
 };
